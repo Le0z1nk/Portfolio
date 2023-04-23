79dd7107625786b7d8c063a0a722760acaf9b795
Le0z1nk <leoamorim132@gmail.com>
2023-04-23 16:09:49 -0300

    arrumando a main

--- src/Components/Main/main.js
+++ src/Components/Main/main.js
@@ -8,21 +8,39 @@ export default function Main() {
           <S.ImgFoto src="../fotoeu.png" alt="foto" />
           <S.CaixaSobre>
           <S.Titulo1>Sobre mim</S.Titulo1>
-          <S.Paragrafo> Olá! Sou Programador Front-End.</S.Paragrafo>
+          <S.Paragrafo> Olá! Me chamo Leonardo e sou programador Front-End.</S.Paragrafo>
           </S.CaixaSobre>
           <S.CaixaHab>
             <S.TituloHab>Habilidades</S.TituloHab>
             <S.Caixaimghab>
               <figure>
                 <img src="../html5.png" alt="html" />
-                <figcaption>HTML5</figcaption>
+                <S.NomeHab1>HTML5</S.NomeHab1>
               </figure>
+              <figure>
                <img src="../css3.png" alt="css" />
+               <S.NomeHab2>CSS3</S.NomeHab2>
+               </figure>
+               <figure>
                <img src="../git.png" alt="git" />
+               <S.NomeHab3>GIT</S.NomeHab3>
+               </figure>
+               <figure>
                <img src="../npm.png" alt="npm" />
+               <S.NomeHab4>NPM</S.NomeHab4>
+               </figure>
+               <figure>
                <img src="../JavaScript-logo.png" alt="javascript" />
+               <S.NomeHab5>JAVASCRIPT</S.NomeHab5>
+               </figure>
+               <figure>
                <img src="../reactimg.png" alt="react" />
+               <S.NomeHab6>REACT</S.NomeHab6>
+               </figure>
+               <figure>
                <img src="../styledcomponents.png" alt="styled components" />
+               <figcaption>STYLED-COMPONENTS</figcaption>
+               </figure>
             </S.Caixaimghab>
           </S.CaixaHab>
         </S.Main>
--- src/Components/Main/main_style.js
+++ src/Components/Main/main_style.js
@@ -4,6 +4,7 @@ export const Main = styled.main `
 display: flex;
 justify-content: space-evenly;
 height: 87vh;
+background-image: url("../Main/fundotec.jpg")
 `
 export const ImgFoto = styled.img `
 width: 23vw;
@@ -15,15 +16,17 @@ margin-top: 6rem;
 `
 export const Titulo1 = styled.h1 `
 font-size: 3rem;
+padding-left: 7rem;
 `
 export const Paragrafo = styled.p `
 font-size: 1.5rem;
 margin-top: 1rem;
+width: 37vw;
+height: 6vh;
 `
 export const CaixaHab = styled.div `
 margin-top: 3rem;
 width: 33.5vw;
-border: solid;
 `
 export const TituloHab = styled.h1 `
 font-size: 5rem;
@@ -33,10 +36,30 @@ display: flex;
 flex-wrap: wrap;
 justify-content: space-evenly;
 img {
-    width: 91px;
-    height: 80px;
+    width: 92px;
+    height: 79px;
     margin-top: 1rem;
-    
-    
 }
+figcaption {
+    font-size: 12px;
+    width: 6.7vw;
+}
+`
+export const NomeHab1 = styled.figcaption `
+padding-left: 1.5rem;
+`
+export const NomeHab2 = styled.figcaption `
+padding-left: 1.8rem;
+`
+export const NomeHab3 = styled.figcaption `
+padding-left: 2rem;
+`
+export const NomeHab4 = styled.figcaption `
+padding-left: 1.7rem;
+`
+export const NomeHab5 = styled.figcaption `
+padding-left: 0.6rem;
+`
+export const NomeHab6 = styled.figcaption `
+padding-left: 1.7rem;
 `
