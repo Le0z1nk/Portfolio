b6d215ac231aad736c04980640f4599c49bcf327
Le0z1nk <leoamorim132@gmail.com>
2023-05-14 17:16:58 -0300

    projetos concluido

--- src/Components/projetos.js
+++ src/Components/projetos.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import Carousel from 'react-elastic-carousel'
 import Pepsi from "./imagens/pepsi.png"
 import Portflow from "./imagens/portflow.png"
+import Witcher from "./imagens/whitcher.png"
+import ToDoList from "./imagens/todolist.png"
 
 export const ContainerProjetos = styled.section `
 background-color: rgb(9, 11, 23); 
@@ -51,14 +53,23 @@ export default function Projetos() {
         <ContainerProjetos>
         <h1> PROJETOS</h1> 
         <Carousel itemsToShow={1}>
-        <ProjectContainer>
-         <a href="https://4qvo4e.csb.app/"><ImgProject src={Pepsi} alt="pepsi page" /></a>
-         <Descricao>Pagina inicial da pepsi. Foi usado HTML5, CSS3, Javascript, React e Styled-components</Descricao>
-         </ProjectContainer>
          <ProjectContainer>
          <a href="https://portflow-kwexu9fek-le0z1nk.vercel.app/"><ImgProject src={Portflow} alt="portflow" /></a>
          <Descricao>Site Portflow. Foi usado HTML5 e CSS3</Descricao>
          </ProjectContainer>
+         <ProjectContainer>
+         <a href="https://the-witcher-zeta.vercel.app/"><ImgProject src={Witcher} alt="the witcher" /></a>
+         <Descricao>Pagina inicial de The Witcher. Foi usado HTML5 e CSS3</Descricao>
+         </ProjectContainer>
+         <ProjectContainer>
+         <a href="https://ugjcpj.csb.app/"><ImgProject src={ToDoList} alt="to do list" /></a>
+         <Descricao>Lista de tarefas. Foi usado HTML5, CSS3, Javascript e React </Descricao>
+         </ProjectContainer>
+         <ProjectContainer>
+         <a href="https://4qvo4e.csb.app/"><ImgProject src={Pepsi} alt="pepsi page" /></a>
+         <Descricao>Pagina inicial da pepsi. Foi usado HTML5, Javascript, React e Styled-components</Descricao>
+         </ProjectContainer>
+         
         </Carousel>
         </ContainerProjetos>      
     )
